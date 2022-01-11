6741de3af616b8f4e8b71fab52fd8768d78e7f22
mdrafiqbd <47newsrafiq@gmail.com>
2022-01-11 19:53:31 +0600

    added backend template

---
+++ Backend/partials/comments.php
@@ -0,0 +1,48 @@
+<div class="card">
+    <div class="card-header">
+        <h4>Latest Comments</h4>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table table-hover table-lg">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Comment</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="col-3">
+                            <div class="d-flex align-items-center">
+                                <div class="avatar avatar-md">
+                                    <img src="assets/images/faces/5.jpg">
+                                </div>
+                                <p class="font-bold ms-3 mb-0">Si Cantik</p>
+                            </div>
+                        </td>
+                        <td class="col-auto">
+                            <p class=" mb-0">Congratulations on your graduation!</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="col-3">
+                            <div class="d-flex align-items-center">
+                                <div class="avatar avatar-md">
+                                    <img src="assets/images/faces/2.jpg">
+                                </div>
+                                <p class="font-bold ms-3 mb-0">Si Ganteng</p>
+                            </div>
+                        </td>
+                        <td class="col-auto">
+                            <p class=" mb-0">Wow amazing design! Can you make
+                                another
+                                tutorial for
+                                this design?</p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
---
+++ Backend/partials/profile_visit.php
@@ -0,0 +1,50 @@
+<div class="card-body">
+    <div class="row">
+        <div class="col-6">
+            <div class="d-flex align-items-center">
+                <svg class="bi text-primary" width="32" height="32" fill="blue" style="width:10px">
+                    <use xlink:href="assets/vendors/bootstrap-icons/bootstrap-icons.svg#circle-fill" />
+                </svg>
+                <h5 class="mb-0 ms-3">Europe</h5>
+            </div>
+        </div>
+        <div class="col-6">
+            <h5 class="mb-0">862</h5>
+        </div>
+        <div class="col-12">
+            <div id="chart-europe"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            <div class="d-flex align-items-center">
+                <svg class="bi text-success" width="32" height="32" fill="blue" style="width:10px">
+                    <use xlink:href="assets/vendors/bootstrap-icons/bootstrap-icons.svg#circle-fill" />
+                </svg>
+                <h5 class="mb-0 ms-3">America</h5>
+            </div>
+        </div>
+        <div class="col-6">
+            <h5 class="mb-0">375</h5>
+        </div>
+        <div class="col-12">
+            <div id="chart-america"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            <div class="d-flex align-items-center">
+                <svg class="bi text-danger" width="32" height="32" fill="blue" style="width:10px">
+                    <use xlink:href="assets/vendors/bootstrap-icons/bootstrap-icons.svg#circle-fill" />
+                </svg>
+                <h5 class="mb-0 ms-3">Indonesia</h5>
+            </div>
+        </div>
+        <div class="col-6">
+            <h5 class="mb-0">1025</h5>
+        </div>
+        <div class="col-12">
+            <div id="chart-indonesia"></div>
+        </div>
+    </div>
+</div>
