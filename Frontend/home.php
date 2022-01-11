2c4e68f6109fa2704d69f002621c1d88cf10c830
mdrafiqbd <47newsrafiq@gmail.com>
2022-01-11 17:27:27 +0600

    frontend tamplating done

---
+++ Frontend/home.php
@@ -0,0 +1,26 @@
+<?php
+require_once "partials/header.php"; ?>
+
+<main>
+    <?php  require_once "home/slider.php";?>
+    <?php  require_once "home/new_products.php";?>
+    <?php  require_once "home/gallery.php";?>
+    <?php  require_once "home/popular.php";?>
+    <?php  require_once "home/video.php";?>
+    <?php  require_once "home/choice.php";?>
+    <?php  require_once "home/shop.php";?>
+    <?php  require_once "home/search.php";?>
+
+
+
+
+
+
+
+
+</main>
+
+
+<?php
+require_once "partials/footer.php";
+?>
